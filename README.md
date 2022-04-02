fffbb4f2d05501c63179aa3f125d02104cc2ee17
adrianen-ucm <adrianen@ucm.es>
2022-04-02 23:01:30 +0200

    representation improved

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Teleport demo
 
+Demo and examples for quantum state teleportation using 
+[Quipper](https://www.mathstat.dal.ca/~selinger/quipper/).
+It works with GHC 8.8.4.
+
+## Exacutable usage examples
+
 ```sh
-cabal run [probability|simulation] -- [0|1|+|-]
+cabal run [probability|simulation] [0|1|+|-]
 ```
 
 ```sh
-cabal run -v0 pdf > example.pdf
+cabal run -v0 pdf > circuit.pdf
 ```
--- src/Teleport.hs
+++ src/Teleport.hs
@@ -1,21 +1,30 @@
 module Teleport (protocol, bell, teleport) where
 
-import           Quipper (Circ, Qubit, controlled, controlled_not,
+import           Quipper (Bit, Circ, Qubit, controlled, controlled_not,
                           controlled_not_at, gate_X_at, gate_Z_at, hadamard,
-                          hadamard_at, qc_init)
+                          hadamard_at, measure, qc_init)
 
-protocol :: Qubit -> Circ ((Qubit, Qubit), Qubit)
+-- | Example circuit of the protocol for teleporting a given qubit.
+protocol :: Qubit -> Circ ((Bit, Bit), Qubit)
 protocol a = do
   (b1, b2) <- qc_init (False, False) >>= bell
   teleport ((a, b1), b2)
 
+-- | Circuit for building Bell states.
 bell :: (Qubit, Qubit) -> Circ (Qubit, Qubit)
 bell (x, y) = hadamard x >>= controlled_not y
 
-teleport :: ((Qubit, Qubit), Qubit) -> Circ ((Qubit, Qubit), Qubit)
+-- | Quantum state teleportation circuit. The first pair of qubits belong to
+-- Alice and the last one to Bob.
+teleport :: ((Qubit, Qubit), Qubit) -> Circ ((Bit, Bit), Qubit)
 teleport ((a, b1), b2) = do
+  -- Alice operates with her qubits
   controlled_not_at b1 a
   hadamard_at a
-  gate_X_at b2 `controlled` b1
-  gate_Z_at b2 `controlled` a
-  pure ((a, b1), b2)
+  -- She measures them and sends the results to Bob
+  (m1, m2) <- measure (a, b1)
+  -- Bob operates with his qubit according to the received information
+  gate_X_at b2 `controlled` m2
+  gate_Z_at b2 `controlled` m1
+  -- Bob's qubit should now be in the state that the first one of Alice was
+  pure ((m1, m2), b2)
