ff270fe183633c0861e7fe461d7fa1c452ac9b25
adrianen-ucm <adrianen@ucm.es>
2022-04-02 17:38:59 +0200

    refactor

--- src/Teleport.hs
+++ src/Teleport.hs
@@ -1,15 +1,14 @@
 module Teleport (protocol, bell, teleport) where
 
-import           Control.Monad ((>=>))
-import           Quipper       (Circ, Qubit, controlled, controlled_not,
-                                controlled_not_at, gate_X, gate_Z_at, hadamard,
-                                hadamard_at, qc_init)
+import           Quipper (Circ, Qubit, controlled, controlled_not,
+                          controlled_not_at, gate_X_at, gate_Z_at, hadamard,
+                          hadamard_at, qc_init)
 
 protocol :: Qubit -> Circ ((Qubit, Qubit), Qubit)
 protocol a = do
   (b1, b2) <- qc_init (False, False) >>= bell
   teleport ((a, b1), b2)
- 
+
 bell :: (Qubit, Qubit) -> Circ (Qubit, Qubit)
 bell (x, y) = hadamard x >>= controlled_not y
 
@@ -17,8 +16,6 @@ teleport :: ((Qubit, Qubit), Qubit) -> Circ ((Qubit, Qubit), Qubit)
 teleport ((a, b1), b2) = do
   controlled_not_at b1 a
   hadamard_at a
-  correct_at b2
+  gate_X_at b2 `controlled` b1
+  gate_Z_at b2 `controlled` a
   pure ((a, b1), b2)
-  where
-    correct_at = (`controlled` b1) . gate_X
-             >=> (`controlled` a ) . gate_Z_at
